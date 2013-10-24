5ebdd608a7d38924ba02d48d1ce5623b826b8295
Setrino <sk@fkdn-lab.com>
2013-10-24 11:12:10 +0200

    Add entry fields

--- js/functions.js
+++ js/functions.js
@@ -12,6 +12,20 @@ $(document).ready(function(){
     $("#answer").css("margin-left", $("#phonetic").width() / 2 - 97 / 2);
     $("#answer").css("margin-top", - 19 / 2);
 
+    $(".search_form").css("margin-left", ( $("#enter").width() - $(".search_form").width() ) / 2 - 20 );
+    $(".search_form").css("margin-top", ( $("#enter").height() - $(".search_form").height() ) / 2 );
+
+    $(".search_output").css("left", ( $("#result").width() - $(".search_output").width() ) / 2 );
+    $(".search_output").css("margin-top", ( $("#result").height() - $(".search_output").height() ) / 2 - 5 );
+
+    $('.clean').click(function(){
+
+        previousValue = '';
+
+        $(".search_form").val(previousValue);
+        $(".search_output").val(previousValue);
+    });
+
     $('.color_text').bind({
 
         mouseenter: function(){
@@ -37,4 +51,23 @@ $(document).ready(function(){
 
         "click", doOverlayClose
     )
+
 });
+
+var previousValue = '';
+
+var checkLoop = function(){
+
+    if($(".search_form").val() != '' || $(".search_form").val() != undefined){
+
+        if(previousValue != $(".search_form").val()){
+
+            previousValue = $(".search_form").val();
+
+            //Enter the AJAX check here
+        }
+    }
+
+    gLoop = setTimeout(checkLoop, 2000);
+}
+checkLoop();
