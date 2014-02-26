14164e9acaf3f24cf10e69c993469cf4564f006d
Setrino <sk@fkdn-lab.com>
2014-02-26 19:26:35 +0100

    Add Read More

--- js/functions.js
+++ js/functions.js
@@ -77,6 +77,8 @@ $(document).ready(function(){
 
     hideFullScreen();
 
+    $('article').readmore({maxHeight: 190});
+    $('.readmore-js-toggle').css('text-align', 'left');
 });
 
 function hideFullScreen(){
