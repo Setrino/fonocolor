3c12b07b085b1fcefc8c2c229a719cd36ab9a2d5
Setrino <sk@fkdn-lab.com>
2014-05-09 11:12:39 +0200

    Add reference, add meta_data.php, add .htaccess

---
+++ php/meta_data.php
@@ -0,0 +1,16 @@
+<title>Phonocolor</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="shortcut icon" href="http://phonocolor.ch/images/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Le site phonocolor permet l'apprentissage de la langue française en utilisant des représentations de couleurs de phonèmes">
+    <meta name="keywords" content="phonocolor, fonocolor, couleurs, jeu, français, phonèmes, graphèmes"/>
+    <meta name="robots" content="index,follow"/>
+    <meta name="author" content="Phonocolor" />
+    <meta http-equiv="expires" content="Sun, 11 Jan 2015 12:00:00 GMT">
+    <meta property="og:site_name" content="Phonocolor"/>
+    <meta property="og:type" content="website.main" />
+    <meta property="og:url" content="http://phonocolor.ch/" />
+    <meta property="og:image" content="http://phonocolor.ch/images/logo.png" />
+    <meta property="og:description" content="Le site phonocolor permet l'apprentissage de la langue française en utilisant des représentations de couleurs de phonèmes" />
+    <link rel="stylesheet" href="css/fonts.css" type="text/css" charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/jquery-1.10.2.min.js"></script>
