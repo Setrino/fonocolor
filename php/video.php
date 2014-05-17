2c8d76b7c306c8ea88c71df5c7ab348cdd319cb2
Setrino <sk@fkdn-lab.com>
2014-05-17 17:24:25 +0200

    Compress HTML

--- php/video.php
+++ php/video.php
@@ -1,7 +1,1 @@
-<div class="video">
-    <a href="#" class="video_skip">Skip video >></a>
-    <video id="videoFile" controls style="width:640px;height:360px;" poster="images/logo.png">
-        <source src=""
-                type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"' />
-    </video>
-</div>
+<div class="video"> <a href="#" class="video_skip">Skip video >></a> <video id="videoFile" controls style="width:640px;height:360px;" poster="images/logo.png"> <source src="" type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'/> </video> </div>
