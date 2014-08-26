60dbf4a929b7ff45986d72aaf9cfc902733c3a78
Setrino <sk@fkdn-lab.com>
2014-08-27 01:27:34 +0200

    Fix

--- php/requests.php
+++ php/requests.php
@@ -800,7 +800,7 @@ if(isset($_POST['filepath'])){
     $audioFiles = scandir($_SERVER['DOCUMENT_ROOT'].$path);
 
     if(sizeof($audioFiles) > 0){
-        echo $_SERVER['DOCUMENT_ROOT'].$path;
+        echo $audioFiles;
     }else{
         echo 'ERROR';
     }
