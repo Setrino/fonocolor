4ba788aa02180706fb7fc38b67b423da4891a702
Setrino <sk@fkdn-lab.com>
2015-03-14 12:15:51 +0100

    Test

--- php/requests.php
+++ php/requests.php
@@ -29,7 +29,7 @@ if(isset($_POST['text'])){
         if($firstLetter){
             $request[0][0] = mb_strtoupper($request[0][0], "UTF-8");
         }
-        echo json_encode($word);
+        echo json_encode($request);
         $firstLetter = false;
 
     }else{
